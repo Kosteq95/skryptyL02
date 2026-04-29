1ba042b6a2d42c3081e32c4936ec02f5702dadac
s8@pakiso <s8@pakiso>
2026-04-29 13:02:44 +0000

    v1502

--- ile_do_konca.sh
+++ ile_do_konca.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 echo "Podaj o której kończysz pracę:"
-read h_end
+read h_end m_end
 
+h_now=$(($(date +%H)+2))
+m_now=$(date +%M)
 
-echo "Do końca pracy o $h_end zostało: $((h_end-($(date +%H)+1))):$((60-$(date +%M)))"
+echo "Aktualna godzina to $h_now:$m_now"
 
+h_left=$((h_end-h_now))
+m_left=$((m_end-m_now))
+
+echo "Do końca pracy o $h_end:$m_end  zostało: $h_left h $m_left min"
 
