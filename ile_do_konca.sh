5255bc16e55928103f3cbbda945592179304176f
s8@pakiso <s8@pakiso>
2026-04-29 12:26:32 +0000

    v0.1

--- ile_do_konca.sh
+++ ile_do_konca.sh
@@ -2,13 +2,12 @@
 
 
 #koniec=16
-#terazh=
+#terazh= sacascascasc
 #terazm=$(date +%M)
-
-#zostalo_h=
 #zostalo_min=$((60-$terazm))
 
 echo "Do końca pracy o 17 zostało: $((17-($(date +%H)+1))):$((60-$(date +%M)))"
+
 
 if [ $1 -eq  1 ] 
 then
