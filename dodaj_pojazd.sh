a1783c37619ca05502cba69c4970f13bb453a287
s8@pakiso <s8@pakiso>
2026-05-06 13:22:22 +0000

    v1522

--- dodaj_pojazd.sh
+++ dodaj_pojazd.sh
@@ -17,4 +17,38 @@
 # Numer rejestracyjny: .......... 
 # Data ostatniego przegladu: DD.MM.YYYY 
 # Przebieg na ostatnim przeglądzie: ...... km 
-# Ostania aktualizacja odbyła się DD.MM.YYYY HH:MM:SS przez "nazwa użytkownika systemu"
+
+typ="osobowe"
+
+if [ "$1" = "-c" ]
+then
+    typ=ciezarowe
+elif [ -n "$1" ] && [ "$1" != "-o" ]
+then
+   echo "Nie poprawny argument!" 1>&2
+   exit 1
+fi
+
+
+
+read -p "Podaj markę samochodu: " marka
+read -p "Podaj model samochodu: " model
+read -p "Podaj rocznik samochodu: " rocznik
+read -p "Podaj numer rejestracyjny samochodu: " nr_rej
+read -p "Podaj datę ostatniego przeglądu i przebieg samochodu: (DD:MM:YYYY) " data_przegladu przebieg
+
+
+dir_path="$HOME/Baza_Pojazdow/$typ/$marka"
+if ! [ -d $dir_path ] 
+then
+ mkdir -p $dir_path
+fi
+
+echo "-------------- CAR PASSPORT ----------------- 
+Marka: $marka
+Model: $model
+Rocznik: $rocznik 
+Numer rejestracyjny: $nr_rej
+Data ostatniego przegladu: $data_przegladu
+Przebieg na ostatnim przeglądzie: $przebieg km 
+Ostania aktualizacja odbyła się $(date +%d.%m.%Y\ %H:%M:%S) przez $USER" > $dir_path/$nr_rej.pc
